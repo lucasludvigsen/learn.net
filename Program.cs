a31cedb58c85791f7a198ca053c2e334e1502e62
Lucas Ludvigsen <lucas+github@gottschlag.se>
2019-12-25 20:22:47 +0100

    new console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
